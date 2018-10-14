6f0b3dead8fed3e2eb863d12b1831297f2e992dd
Unknown <lgmpf@icomp.ufam.edu.br>
2018-10-14 09:07:53 -0400

    mapinhaEnganador, googleMaps API

--- Monitorai/src/pages/home/home.ts
+++ Monitorai/src/pages/home/home.ts
@@ -1,3 +1,13 @@
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { OnibusPage } from '../onibus/onibus';
@@ -7,7 +17,7 @@ import { OnibusPage } from '../onibus/onibus';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  map: GoogleMap;
   constructor(public navCtrl: NavController) {
 
   }
@@ -15,5 +25,45 @@ export class HomePage {
   verOnibus(numeroLinha) {
     this.navCtrl.push(OnibusPage,{linha: numeroLinha});
   }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDqYpD9wyD7wiWnyJ12nhgKUuVaVwdqw5w',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDqYpD9wyD7wiWnyJ12nhgKUuVaVwdqw5w'
+    });
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: -3.092570,
+           lng: -60.018327
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: -3.092570,
+        lng: -60.018327
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
+}
 
 }
